51756eb94dae5d0fa87409bd276c88216203eb1b
yoitsmedarls <johndaryljavier26@gmail.com>
2024-12-04 22:38:35 +0800

    Test for sync changes

--- app/include/Substance/Liquid.hpp
+++ app/include/Substance/Liquid.hpp
@@ -3,9 +3,10 @@
 
 #include "Substance.hpp"
 
-/** @author @yoitsmedarls
- * @brief A child of the Substance class that defines the liquids that will be placed
- * inside of containers (i.e. water).
+/**
+ * @author @yoitsmedarls
+ * @brief A child of the Substance class that defines the liquids that will be
+ *        placed inside of containers (i.e. water).
  */
 class Liquid : public Substance
 {
@@ -14,26 +15,61 @@ private: // Class fields
     double m_freezingPoint;
 
 public: // Special methods
-    /// @brief Default constructor, initializes every field with 0 or its equivalent
+    /**
+     * @brief   Construct a new Liquid object.
+     * @details Serves as the default constructor. It initializes every field
+     *          with 0 or its equivalent.
+     */
     Liquid();
 
-    /// @brief Constructor with arguments for the class fields. Since the object has just been constructed,
-    /// it is not contained in a Container by default.
+    /**
+     * @brief   Construct a new Liquid object with supplied parameters
+     * @details Constructor with arguments for the class fields. Since the
+     *          object has just been constructed, it is not contained in a
+     *          Container by default.
+     *
+     * @param name                 Give the substance a name for identification
+     *                             purposes. Used in debugging.
+     * @param mass                 Substances must have a mass.
+     * @param temperature          Key parameter given that the simulated
+     *                             experiment is a specific heat experiment.
+     * @param specificHeatCapacity Refer to https://en.wikipedia.org/wiki/Table_of_specific_heat_capacities
+     *                             for the specific heat capacity of your chosen
+     *                             substance.
+     * @param density              The mass and density values supplied in this
+     *                             constructor is used to calculate volume.
+     * @param boilingPoint         Unique to the Liquid class. Used for the
+     *                             simulation of heating the liquid in a
+     *                             Container using a HeatPlate.
+     * @param freezingPoint        Not really useful for the simulation,
+     *                             but who cares :P
+     */
     Liquid(std::string name, double mass, double temperature, double specificHeatCapacity, double density,
            double boilingPoint, double freezingPoint);
 
-    /// @brief Copy constructor. Handles cases where you pass a Liquid object to the Liquid constructor.
-    /// @param copy
+    /**
+     * @brief   Copy constructor of the Liquid class.
+     * @details Constructs a Liquid object as a copy of another one.
+     *
+     * @param copy A pre-existing, pre-initialized Liquid object that is
+     *             passed by reference.
+     */
     Liquid(const Liquid &copy);
 
-    /// @brief Destructor
+    /**
+     * @brief Destroy the Substance object.
+     */
     ~Liquid();
 
 public: // Operator overloads
-    /// @brief Copy assignment operator. Handles cases where you assign a blank Liquid object with another
-    /// pre-existing Liquid object. It prevents extra copies.
-    /// @param copy
-    /// @return
+    /**
+     * @brief   Copy assignment operator of the Liquid class.
+     * @details Handles cases where you assign a blank Liquid object with
+     *          another pre-existing Liquid object. It prevents extra copies.
+     *
+     * @param copy
+     * @return Liquid&
+     */
     inline Liquid &operator=(const Liquid &);
 
 public: // Getters and Setters
--- app/include/Substance/Substance.hpp
+++ app/include/Substance/Substance.hpp
@@ -5,8 +5,8 @@
 
 /**
  * @author @yoitsmedarls
- * @brief The base class for all objects designed to be 'placed' or 'put inside'
- * objects from (or derived from) the Container class.
+ * @brief  The base class for all objects designed to be 'placed' or
+ *         'put inside' objects from (or derived from) the Container class.
  */
 class Substance
 {
@@ -22,43 +22,50 @@ protected: // Class fields
 public: // Special methods
     /**
      * @brief   Construct a new Substance object.
-     * @details Serves as the default constructor. It initializes every field with 0 or its equivalent.
+     * @details Serves as the default constructor. It initializes every field
+     *          with 0 or its equivalent.
      */
     Substance();
 
     /**
      * @brief   Construct a new Substance object with supplied parameters
-     * @details Constructor with arguments for the class fields.Since the object has just been constructed,
-     *          it is not contained in a Container by default.
+     * @details Constructor with arguments for the class fields. Since the
+     *          object has just been constructed, it is not contained in a
+     *          Container by default.
      *
-     * @param name                 Give the substance a name for identification purposes. Used in debugging.
+     * @param name                 Give the substance a name for identification
+     *                             purposes. Used in debugging.
      * @param mass                 Substances must have a mass.
-     * @param temperature          Key parameter given that the simulated experiment is a specific heat experiment.
-     * @param specificHeatCapacity Refer to
-     *                             @link https://en.wikipedia.org/wiki/Table_of_specific_heat_capacities @endlink
-     *                             for the specific heat capacity of your chosen substance.
-     * @param density              The mass and density values supplied in this constructor is used to calculate volume.
+     * @param temperature          Key parameter given that the simulated
+     *                             experiment is a specific heat experiment.
+     * @param specificHeatCapacity Refer to https://en.wikipedia.org/wiki/Table_of_specific_heat_capacities
+     *                             for the specific heat capacity of your chosen
+     *                             substance.
+     * @param density              The mass and density values supplied in this
+     *                             constructor is used to calculate volume.
      */
-    Substance(std::string name, double mass, double temperature, double specificHeatCapacity, double density);
+    Substance(std::string name, double mass, double temperature,
+              double specificHeatCapacity, double density);
 
     /**
-     * @brief Copy constructor. Constructs a Substance object as a copy of another one.
+     * @brief   Copy constructor of the Substance class.
+     * @details Constructs a Substance object as a copy of another one.
      *
-     * @param copy A pre-existing, pre-initialized Substance object that is passed by reference.
+     * @param copy A pre-existing, pre-initialized Substance object that is
+     *             passed by reference.
      */
     Substance(const Substance &copy);
 
     /**
-     * @brief Destroy the Substance object
-     *
+     * @brief Destroy the Substance object.
      */
     ~Substance();
 
 public: // Operator overloads
     /**
-     * @brief   Copy assignment operator.
-     * @details Handles cases where you assign a blank Substance object with another
-     *          pre-existing Substance object. It prevents extra copies.
+     * @brief   Copy assignment operator of the Substance class.
+     * @details Handles cases where you assign a blank Substance object with
+     *          another pre-existing Substance object. It prevents extra copies.
      *
      * @param copy
      * @return Substance&
@@ -90,14 +97,15 @@ public: // Other methods
 
     /**
      * @brief   Calculates volume from the Substance's mass and density values.
-     * @details Updates the volume of the Substance upon changes to the other aforementioned fields.
-     *          Usually called inside other methods that change the value of fields (i.e. setters).
+     * @details Updates the volume of the Substance upon changes to the other
+     *          aforementioned fields. Usually called inside other methods that
+     *          change the value of fields (i.e. setters).
      */
     void updateVolume();
 
     /**
-     * @brief  For debugging. Prints out the properties of the class in the console
-     *
+     * @brief  For debugging. Prints out the properties of the class in the
+     *         console for review.
      */
     void printProperties();
 };
